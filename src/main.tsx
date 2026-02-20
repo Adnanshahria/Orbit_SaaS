895dede3a63e0ab6db661f117dc1388af0df2cc3
Adnanshahria <adnanshahria2019@gmail.com>
2026-02-21 01:37:16 +0600

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,31 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Lock console output
+const originalConsole = {
+    log: console.log,
+    error: console.error,
+    warn: console.warn,
+    info: console.info,
+    debug: console.debug,
+    trace: console.trace,
+    dir: console.dir,
+    table: console.table,
+};
+
+(window as any).__unlockConsole = () => {
+    Object.assign(console, originalConsole);
+    console.log("Console unlocked!");
+};
+
+console.log = () => { };
+console.error = () => { };
+console.warn = () => { };
+console.info = () => { };
+console.debug = () => { };
+console.trace = () => { };
+console.dir = () => { };
+console.table = () => { };
+
+
 createRoot(document.getElementById("root")!).render(<App />);
